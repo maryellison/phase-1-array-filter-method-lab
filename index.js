1012fe7ddb7985675148205454ee7effc98bfa6b
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-06 11:15:05 -0500

    commit tree 7157d767fda7673a8448e67ce4a0a07a66f977b1

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution here
+//function findMatching(array, str){
+//    const newMatch = [];
+//    for (const user of array) {
+//        if (user.toLowerCase() === str.toLowerCase()){
+//            newMatch.push(user);
+//        } else if (user.toLowerCase() !== str.toLowerCase()){
+//            return ([]);
+//        }
+//        return (newMatch)
+//    }
+//    }
+function findMatching(drivers, string){
+    let result = drivers.filter(element => element.toLowerCase() === string.toLowerCase())
+    return result
+}
+
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+//this is correct, it runs. But very wordy.
+function fuzzyMatch(array, str){
+    const newArray = [];
+      for (const user of array) {
+          if (user.charAt(0) === str.charAt(0) && user.charAt(1) === str.charAt(1)){
+              newArray.push(user)
+          }
+      }
+    return (newArray)
+  }
+
+//Problem with the code below, but closer to a cleaner, correct code from Slack
+//  function fuzzyMatch(drivers, string){
+//    let result = drivers.filter(word => string === word.startsWith('Sa'))
+//    return result
+//  }
+
+
+//  function matchName(array, str){
+//    const information = [];
+//    for (const user of array){
+//        if (str === user){
+//            information.push(user)
+//        }
+//    }
+//  }
+
+  function matchName(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.name === name
+    })
+  }
